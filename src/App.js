94ad5dc13f0b88d946206fcb6c71c17e4d5fa07f
Vinh Le <31971259+vinhlee95@users.noreply.github.com>
2020-04-12 13:21:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
+import "./tailwind.css";
 
 export default function App() {
-  return <div className="App" />;
+  return <div className="App text-red-300">App</div>;
 }
